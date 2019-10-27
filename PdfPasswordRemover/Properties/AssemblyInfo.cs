ba2c1c06c4403d6429a87f605203bdaa9789c1a9
SeppPenner <HansM2013@gmx.de>
2019-10-27 20:34:25 +0100

    Updated nuget packages, added GitVersionTask.

--- PdfPasswordRemover/Properties/AssemblyInfo.cs
+++ PdfPasswordRemover/Properties/AssemblyInfo.cs
@@ -20,16 +20,3 @@ using System.Runtime.InteropServices;
 
 // Die folgende GUID bestimmt die ID der Typbibliothek, wenn dieses Projekt für COM verfügbar gemacht wird
 [assembly: Guid("1b571581-9528-4843-a325-f98a56452bb3")]
-
-// Versionsinformationen für eine Assembly bestehen aus den folgenden vier Werten:
-//
-//      Hauptversion
-//      Nebenversion
-//      Buildnummer
-//      Revision
-//
-// Sie können alle Werte angeben oder Standardwerte für die Build- und Revisionsnummern verwenden,
-// übernehmen, indem Sie "*" eingeben:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.1")]
-[assembly: AssemblyFileVersion("1.0.0.1")]
--- README.md
+++ README.md
@@ -25,5 +25,6 @@ PdfPasswordRemover is a project to remove passwords from PDF files. The project 
 Change history
 --------------
 
+* **Version 1.0.1.0 (2019-10-27)** : Updated nuget packages, added GitVersionTask.
 * **Version 1.0.0.1 (2019-05-07)** : Updated .Net version to 4.8.
 * **Version 1.0.0.0 (2018-10-22)** : 1.0 release.
